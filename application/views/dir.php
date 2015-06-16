472cfb79ea96317ab66a80ee6a6bcd590c19c56a
gaber <maruf.zaber@gmail.com>
2015-06-16 06:08:56 +0600

    sf

--- application/views/dir.php
+++ application/views/dir.php
@@ -3,13 +3,13 @@
 
 <html>;
 
-    <h1><a href="https://floating-fjord-4628.herokuapp.com/index.php/welcome/greetings?q=Hi! how are you?">
+    <h1><a href="https://floating-fjord-4628.herokuapp.com/index.php/Muktosoft/greetings?q=Hi! how are you?">
             Greetings</a></h1>;
 
-    <h1><a href="https://floating-fjord-4628.herokuapp.com/index.php/welcome/weather?q=What is today's humidity in Dhaka?">
+    <h1><a href="https://floating-fjord-4628.herokuapp.com/index.php/Muktosoft/weather?q=What is today's humidity in Dhaka?">
             Weather</a></h1>;
 
-    <h1><a href="https://floating-fjord-4628.herokuapp.com/index.php/welcome/qa?q=What is today's humidity in Dhaka?">
+    <h1><a href="https://floating-fjord-4628.herokuapp.com/index.php/Muktosoft/qa?q=What is today's humidity in Dhaka?">
             Question And Answer</a></h1>;
 
 </html>;
